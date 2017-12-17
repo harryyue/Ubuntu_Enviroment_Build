60692c246ac0c7df7359c272ae989afa03c16bc5
Harry Yue <harryyue123@163.com>
2017-12-18 01:57:20 +0800

    Add the terminal tools---minicom into install.sh script
    Signed-off-by: Harry Yue <harryyue123@163.com>

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ Download this directory,and run the install.sh script,and it will download
 necessary tools and build a ubuntu environment.
 	
 	Usage: 
-		bash install.sh (root_pwd)
+		bash install.sh [root_pwd]
 
 # Which tools will be installed?
 After run install.sh script,we will install and configure below tools:
-	1.git and gitk
+	1.git ,gitg and gitk
 	2.powerline
-	3.vim and ctags
+	3.vim ,minicom and ctags
 	4.ssh-server
 	5.tmux
 	6.kscope 1.6.2 ,cscope and graphviz
--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ introduction()
 	echo "###  Email:     harryyue123@163.com"
 	echo "###  Version:   14.04_V1.0"
 	echo "###  Create:    2017-09-01"
-	echo "###  Update:    2017-09-04"
+	echo "###  Update:    2017-12-18"
 	echo "###  Description："
 	echo "###  We can auto-install below tools by this script,if also"
 	echo "###  need install other tools,you can update this script to "
@@ -25,7 +25,7 @@ introduction()
 	echo "###  Tools:"
 	echo "###  	 1.git，gitk and gitg"
 	echo "###  	 2.powerline"
-	echo "###  	 3.vim and ctags"
+	echo "###  	 3.vim , minicom and ctags"
 	echo "###  	 4.ssh server"
 	echo "###  	 5.tmux"
 	echo "###  	 6.kscope 1.6.2 ,cscope and graphviz"
@@ -39,7 +39,7 @@ cd $TOPDIR
 echo -e $GREEN "Starting to install..." $RESET
 
 #Install and configure the git
-echo "[1/]Begin to install git and gitk..."
+echo "[1/6]Begin to install git and gitk..."
 echo $1 | sudo -S apt-get -y install git gitk gitg
 cp gitconfig ~/.gitconfig 
 if [ $? != 0 ]
@@ -47,10 +47,10 @@ then
 	echo -e $RED "[ERROR] Can't find gitconfig..." $RESET
 	exit 1
 fi
-echo "[1/]done."
+echo "[1/6]done."
 
 #Install and apply for powerline
-echo "[2/]Begin to install powerline ..."
+echo "[2/6]Begin to install powerline ..."
 echo $1 | sudo -S apt-get -y install python-pip
 pip install git+git://github.com/powerline/powerline
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -58,11 +58,11 @@ wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbol
 echo $1 | sudo -S mv PowerlineSymbols.otf /usr/share/fonts/
 fc-cache -vf /usr/share/fonts/
 echo $1 | sudo -S  mv 10-powerline-symbols.conf /etc/fonts/conf.d/
-echo "[2/]done."
+echo "[2/6]done."
 
 #Install and configure vim
-echo "[3/]Begin to install vim and ctags ..."
-echo $1 | sudo -S apt-get -y instll vim ctags
+echo "[3/6]Begin to install vim and ctags ..."
+echo $1 | sudo -S apt-get -y instll vim ctags minicom
 cd vimconfig
 cp -rf * ~
 if [ $? != 0 ]
@@ -71,15 +71,15 @@ then
 	exit 1
 fi
 cd -
-echo "[3/]done."
+echo "[3/6]done."
 
 #Install ssh server
-echo "[4/]Begin to install ssh-server..."
+echo "[4/6]Begin to install ssh-server..."
 echo $1 | sudo -S apt-get -y install openssh-server
-echo "[4/]done."
+echo "[4/6]done."
 
 #Install and tmux tool
-echo "[5/]Install tmux..."
+echo "[5/6]Install tmux..."
 echo $1 | sudo -S apt-get -y install tmux
 cp tmux.conf ~/.tmux.conf 
 if [ $? != 0 ]
@@ -87,14 +87,14 @@ then
 	echo -e $RED "[ERROR] Can't find tmux.conf..." $RESET
 	exit 1
 fi
-echo "[5/]done."
+echo "[5/6]done."
 
 #Install kscope 1.6.2
-echo "[6/]Install kscope..."
+echo "[6/6]Install kscope..."
 echo $1 | sudo -S add-apt-repository ppa:fbirlik/kscope
 echo $1 | sudo -S apt-get update
 echo $1 | sudo -S apt-get -y install kscope-trinity cscope graphviz
-echo "[6/]done."
+echo "[6/6]done."
 
 echo -e $GREEN "Finish..." $RESET
 exit 0
